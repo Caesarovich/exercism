439f88081ee1f107a98ac066e4db4a4140afeeac
Caesarovich <cesar@caesarovich.xyz>
2024-05-30 03:20:58 +0200

    Polish last exercise

--- javascript/poetry-club-door-policy/door-policy.js
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -60,7 +60,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  return (
-    word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase() + ', please'
-  );
+  return frontDoorPassword(word) + ', please';
 }
