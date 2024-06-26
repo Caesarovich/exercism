e9299f18eb0d7ae1700c693cd8973e891011a5f2
Caesarovich <cesar@caesarovich.xyz>
2024-06-26 02:59:57 +0200

    Finished javascript : simple-cipher

---
+++ javascript/simple-cipher/simple-cipher.js
@@ -0,0 +1,56 @@
+//
+// This is only a SKELETON file for the 'Simple Cipher' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Cipher {
+  #key = 'ddddddddddddddddd';
+
+  constructor(key) {
+    if (key) this.#key = key;
+  }
+
+  shift(char, idx) {
+    const keyValue = this.#key.charCodeAt(idx % this.#key.length) - 97;
+    const charValue = char.charCodeAt(0) - 97;
+
+    return String.fromCharCode(((charValue + keyValue) % 26) + 97);
+  }
+
+  unshift(char, idx) {
+    const keyValue = this.#key.charCodeAt(idx % this.#key.length) - 97;
+    const charValue = char.charCodeAt(0) - 97;
+
+    let shift = charValue - keyValue;
+
+    if (shift < 0) {
+      shift += 26;
+    }
+
+    return String.fromCharCode(shift + 97);
+  }
+
+  encode(text) {
+    const encodedChars = [];
+
+    for (let i = 0; i < text.length; i++) {
+      encodedChars.push(this.shift(text[i], i));
+    }
+
+    return encodedChars.join('');
+  }
+
+  decode(text) {
+    const decodedChars = [];
+
+    for (let i = 0; i < text.length; i++) {
+      decodedChars.push(this.unshift(text[i], i));
+    }
+
+    return decodedChars.join('');
+  }
+
+  get key() {
+    return this.#key;
+  }
+}
