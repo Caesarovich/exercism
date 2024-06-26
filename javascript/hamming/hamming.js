05acd24f0acfacb4ced2ce1cb28207da8adb02da
Caesarovich <cesar@caesarovich.xyz>
2024-06-26 03:08:00 +0200

    Finished javascript : hamming

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const compute = (a, b) => {
+  if (a.length !== b.length) throw new Error('strands must be of equal length');
+
+  return a.split('').filter((v, i) => v !== b[i]).length;
+};
