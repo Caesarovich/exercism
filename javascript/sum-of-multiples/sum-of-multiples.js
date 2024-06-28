214744e64dee6c4599f49cd283b7f4faf51d88e0
Caesarovich <cesar@caesarovich.xyz>
2024-06-28 04:19:29 +0200

    Finished javascript : sum-of-multiples

---
+++ javascript/sum-of-multiples/sum-of-multiples.js
@@ -0,0 +1,26 @@
+//
+// This is only a SKELETON file for the 'Sum Of Multiples' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+function findMultiples(base, max) {
+  if (base === 0) return [0];
+
+  const multiples = [];
+
+  for (let i = base; i < max; i += base) {
+    multiples.push(i);
+  }
+
+  return multiples;
+}
+
+export const sum = (itemsBaseValues, level) => {
+  const uniqueMultiples = new Set();
+
+  itemsBaseValues.forEach((base) => {
+    findMultiples(base, level).forEach((a) => uniqueMultiples.add(a));
+  });
+
+  return Array.from(uniqueMultiples).reduce((acc, cur) => acc + cur, 0);
+};
