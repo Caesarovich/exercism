8e149349ac911b70893a4f9da2b2cebe1d9cd26c
Caesarovich <cesar@caesarovich.xyz>
2024-06-28 03:48:16 +0200

    Finished javascript : rectangles

---
+++ javascript/all-your-base/all-your-base.js
@@ -0,0 +1,30 @@
+//
+// This is only a SKELETON file for the 'All Your Base' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const convert = (digits, baseFrom, baseTo) => {
+  if (baseFrom <= 1) throw new Error('Wrong input base');
+  if (baseTo <= 1) throw new Error('Wrong output base');
+
+  if ((digits[0] === 0 && digits.length > 1) || digits.length === 0)
+    throw new Error('Input has wrong format');
+
+  let number = 0;
+
+  for (let i = 0; i < digits.length; i++) {
+    if (digits[i] >= baseFrom || digits[i] < 0)
+      throw new Error('Input has wrong format');
+
+    number += digits[i] * Math.pow(baseFrom, digits.length - i - 1);
+  }
+
+  const newDigits = [];
+
+  do {
+    newDigits.unshift(number % baseTo);
+    number = Math.floor(number / baseTo);
+  } while (number > 0);
+
+  return newDigits;
+};
---
+++ javascript/rectangles/rectangles.js
@@ -0,0 +1,67 @@
+//
+// This is only a SKELETON file for the 'Rectangles' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const CORNER_CHAR = '+';
+const HORIZONTAL_CHAR = '-';
+const VERTICAL_CHAR = '|';
+
+function isValidHorizontalChar(char) {
+  return [CORNER_CHAR, HORIZONTAL_CHAR].includes(char);
+}
+
+function isValidVerticalChar(char) {
+  return [CORNER_CHAR, VERTICAL_CHAR].includes(char);
+}
+
+function isOutOfBounds(rows, x, y) {
+  return y >= rows.length || x >= rows[0].length;
+}
+
+function exploreDownward(rows, originX, targetX, y, rectCount) {
+  if (
+    isOutOfBounds(rows, targetX, y) ||
+    !isValidVerticalChar(rows[y][originX]) ||
+    !isValidVerticalChar(rows[y][targetX])
+  )
+    return rectCount;
+
+  if (rows[y][originX] === CORNER_CHAR && rows[y][targetX] === CORNER_CHAR)
+    rectCount++;
+
+  return exploreDownward(rows, originX, targetX, y + 1, rectCount);
+}
+
+function exploreRightward(rows, originX, originY, x, rectCount) {
+  if (
+    isOutOfBounds(rows, x, originY) ||
+    !isValidHorizontalChar(rows[originY][x])
+  )
+    return rectCount;
+
+  if (rows[originY][x] === CORNER_CHAR)
+    rectCount += exploreDownward(rows, originX, x, originY + 1, 0);
+
+  return exploreRightward(rows, originX, originY, x + 1, rectCount);
+}
+
+export function count(rows) {
+  let rectCount = 0;
+
+  for (const y in rows) {
+    for (const x in rows[y]) {
+      if (rows[y][x] === CORNER_CHAR) {
+        rectCount += exploreRightward(
+          rows,
+          parseInt(x),
+          parseInt(y),
+          parseInt(x) + 1,
+          0
+        );
+      }
+    }
+  }
+
+  return rectCount;
+}
