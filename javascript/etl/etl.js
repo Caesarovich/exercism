231d54c09c6a190668d9b1e56efa5f4be43f1c6d
Caesarovich <cesar@caesarovich.xyz>
2024-07-01 02:11:52 +0200

    Finished javascript : etl

---
+++ javascript/etl/etl.js
@@ -0,0 +1,16 @@
+//
+// This is only a SKELETON file for the 'ETL' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const transform = (old) => {
+  const newScores = {};
+
+  for (const [score, letters] of Object.entries(old)) {
+    letters.forEach((l) => {
+      newScores[l.toLowerCase()] = parseInt(score);
+    });
+  }
+
+  return newScores;
+};
