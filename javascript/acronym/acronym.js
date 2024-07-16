1f65d06b6685c1b98d7c46f49d13f6f22c011e9c
Caesarovich <cesar@caesarovich.xyz>
2024-07-16 18:34:47 +0200

    Finished javascript : acronym

---
+++ javascript/acronym/acronym.js
@@ -0,0 +1,13 @@
+//
+// This is only a SKELETON file for the 'Acronym' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const parse = (phrase) => {
+  return phrase
+    .match(/[a-z\s-]+/gi)
+    .join('')
+    .split(/[\s-]+/gi)
+    .map((word) => word[0].toUpperCase())
+    .join('');
+};
