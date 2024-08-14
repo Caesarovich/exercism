1a71ddc2e12adc97629dee066b3a400b837e19ab
Caesarovich <cesar@caesarovich.xyz>
2024-08-14 04:28:03 +0200

    Finished javascript : high-scores

---
+++ javascript/high-scores/high-scores.js
@@ -0,0 +1,28 @@
+//
+// This is only a SKELETON file for the 'High Scores' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class HighScores {
+  #scores = [];
+
+  constructor(scores) {
+    this.#scores = scores;
+  }
+
+  get scores() {
+    return this.#scores;
+  }
+
+  get latest() {
+    return this.#scores.at(-1);
+  }
+
+  get personalBest() {
+    return this.#scores.toSorted((a, b) => b - a).at(0);
+  }
+
+  get personalTopThree() {
+    return this.#scores.toSorted((a, b) => b - a).slice(0, 3);
+  }
+}
