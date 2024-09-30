84de3bd8dc297478283d9b79db33af89968de0cc
Caesarovich <cesar@caesarovich.xyz>
2024-09-30 07:34:11 +0200

    Commit lf

---
+++ javascript/knapsack/knapsack.js
@@ -0,0 +1,24 @@
+//
+// This is only a SKELETON file for the 'Knapsack' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const knapsack = (maximumWeight, items) => {
+  items = items.map((i) => ({ ...i, g: i.value / i.weight }));
+
+  items = items.sort((a, b) => b.g - a.g);
+
+  console.log(items);
+
+  let totalValue = 0;
+  let totalWeight = 0;
+
+  for (const item of items) {
+    // console.log(totalValue, item.w, totalValue + item.value > maximumWeight);
+    if (totalWeight + item.weight > maximumWeight) continue;
+    totalValue += item.value;
+    totalWeight += item.weight;
+  }
+
+  return totalValue;
+};
---
+++ javascript/promises/promises.js
@@ -0,0 +1,54 @@
+//
+// This is only a SKELETON file for the 'Promises' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const promisify = (fn) =>
+  fn((error, data) => {
+    return new Promise((res, rej) => {
+      if (error) rej(error);
+      else res(data);
+    });
+  });
+
+export const all = (promises) => {
+  return new Promise((res, rej) => {
+    const finishedPromises = [];
+
+    for (const promiseIdx in promises) {
+      promises[promiseIdx]
+        .then((data) => {
+          finishedPromises[promiseIdx] = data;
+
+          if (
+            finishedPromises.length === promises.length &&
+            !finishedPromises.includes(undefined)
+          )
+            res(finishedPromises);
+        })
+        .catch((err) => rej(err));
+    }
+  });
+};
+
+export const allSettled = () => {
+  throw new Error('Remove this statement and implement this function');
+};
+
+export const race = (promises) => {
+  return new Promise((res, rej) => {
+    for (const promise of promises) {
+      promise
+        .then((data) => {
+          res(data);
+        })
+        .catch((err) => {
+          rej(err);
+        });
+    }
+  });
+};
+
+export const any = () => {
+  throw new Error('Remove this statement and implement this function');
+};
