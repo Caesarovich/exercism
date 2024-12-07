d82f68c0c3ece0443f2310f88f27312661837248
Caesarovich <38408878+Caesarovich@users.noreply.github.com>
2024-12-07 15:28:22 +0000

    Finished javascript : perfect-numbers

---
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Perfect Numbers' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const classify = (n) => {
+  if ( n <= 0 ) throw new Error('Classification is only possible for natural numbers.')
+
+  let sum = 0;
+
+  for (let i = 1; i <= n / 2; i++) 
+    if (n % i === 0) 
+      sum += i;
+
+  if (sum > n) return 'abundant'
+  else if (sum < n) return 'deficient'
+  else return 'perfect'
+};
