745c1d40a9d6f7ed632289d34fbbcb7a2f9a699c
Caesarovich <38408878+Caesarovich@users.noreply.github.com>
2024-12-21 11:54:26 +0000

    Finished javascript : series

--- javascript/series/series.js
+++ javascript/series/series.js
@@ -3,6 +3,32 @@
 // convenience to get you started writing code faster.
 //
 
+import { _ } from "core-js";
+
+
+/// Normal one
+// export class Series {
+//   constructor(series) {
+//     if (series.length === 0) throw new Error('series cannot be empty');
+//     this.series = series;
+//   }
+
+//   slices(sliceLength) {
+//     if (sliceLength < 0) throw new Error('slice length cannot be negative');
+//     if (sliceLength === 0) throw new Error('slice length cannot be zero');
+//     if (this.series.length < sliceLength) throw new Error('slice length cannot be greater than series length');
+
+//     const slices = [];
+
+//     for (let i = 0; i + sliceLength <= this.series.length; i++) {
+//       slices.push(this.series.slice(i, i + sliceLength).split('').map(Number));
+//     }
+
+//     return slices;
+//   }
+// }
+
+/// Functional way
 export class Series {
   constructor(series) {
     if (series.length === 0) throw new Error('series cannot be empty');
@@ -13,13 +39,10 @@ export class Series {
     if (sliceLength < 0) throw new Error('slice length cannot be negative');
     if (sliceLength === 0) throw new Error('slice length cannot be zero');
     if (this.series.length < sliceLength) throw new Error('slice length cannot be greater than series length');
-      
-    const slices = [];
-    
-    for (let i = 0; i + sliceLength <= this.series.length; i++) {
-      slices.push(this.series.slice(i, i + sliceLength).split('').map(Number));
-    }
 
-    return slices;
+    return Array.from(
+      { length: this.series.length - sliceLength + 1},
+      (_, i) => this.series.slice(i, i + sliceLength).split('').map(Number)
+    );
   }
 }
