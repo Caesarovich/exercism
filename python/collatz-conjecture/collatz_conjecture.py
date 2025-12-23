b7cdbd19ed54999bc6b2f73d5a2b17339b3adeb4
Caesarovich <cesar@caesarovich.xyz>
2025-12-23 12:19:54 +0100

    Finish python - collatz

---
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,14 @@
+def steps(number: int) -> int:
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    count: int = 0
+
+    while number > 1:
+        count += 1
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = number * 3 + 1
+
+    return count
